46552c776ae25bc35c0f414d3195036b44ae8663
Heechan Kang <weston0713@gmail.com>
2022-07-13 12:49:36 +0900

    refactoring 1-8

--- ETC/Refactoring/01_first_example/01_8_statement.js
+++ ETC/Refactoring/01_first_example/01_8_statement.js
@@ -2,7 +2,12 @@ const invoice = require("./data/invoices.json")
 const plays = require("./data/plays.json")
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch(aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy" : return new ComedyCalculator(aPerformance, aPlay)
+        default:
+            throw new Error(`알 수 없는 장르: $`)
+    }
 }
 
 class PerformanceCalculator {
@@ -11,25 +16,7 @@ class PerformanceCalculator {
         this.play = aPlay;
     }
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (this.performances.audience > 30) {
-                    result += 1000 * (this.performances.audience - 30);
-                }
-                break;
-            case "comedy": // 희극
-                result = 30000;
-                if (this.performances.audience > 20) {
-                    result += 10000 + 500 * (this.performances.audience - 20);
-                }
-                result += 300 * this.performances.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${this.play.type}`);
-        }
-        return result;
+        throw new Error('서브클래스에서 처리하도록 설계됨.');
     }
 
     get volumeCredits() {
@@ -37,6 +24,27 @@ class PerformanceCalculator {
         result += Math.max(this.performances.audience - 30, 0);
         if ("comedy" === this.play.type)
             result += Math.floor(this.performances.audience / 5);
+        return result
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performances.audience > 30) {
+            result += 1000 * (this.performances.audience - 30);
+        }
+        return result
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performances.audience > 20) {
+            result += 10000 + 500 * (this.performances.audience - 20);
+        }
+        result += 300 * this.performances.audience
         return result
     }
 }
