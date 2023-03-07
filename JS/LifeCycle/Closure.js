1ad2f9650553accd62176f3c411f01296861f29e
HC-kang <weston0713@gmail.com>
2023-03-07 09:40:20 +0900

    JS 클로저

---
+++ JS/LifeCycle/Closure.js
@@ -0,0 +1,26 @@
+function makeUnit(unit) {
+  return function (value) {
+    return (`${value}${unit}`);
+  }
+}
+
+
+let px = makeUnit('px');
+let em = makeUnit('em');
+
+console.log(px(12));
+console.log(px(23));
+console.log(em(34));
+console.log(em(45));
+
+// ---------------------------------------------
+
+let mkUnit = unit => value => (`${value}${unit}`)
+
+let px2 = makeUnit('px');
+let em2 = makeUnit('em');
+
+console.log(px2(122));
+console.log(px2(232));
+console.log(em2(342));
+console.log(em2(452));
---
+++ JS/LifeCycle/Private.js
@@ -0,0 +1,51 @@
+let person = {
+  _bloodTypeList: ["A", "B", "O", "AB"],
+  _bloodType: "A",
+
+  set bloodType(type) {
+    if (this._bloodTypeList.includes(type)) {
+      this._bloodType = type;
+    }
+  },
+
+  get bloodType() {
+    return this._bloodType;
+  },
+};
+
+console.log(person.bloodType);
+person.bloodType = "AB"; // 의도된 변경
+console.log(person.bloodType);
+person.bloodType = "C";  // 의도된 변경금지
+console.log(person.bloodType);
+person._bloodType = "C"; // 의도하지 않은 변경
+console.log(person.bloodType);
+
+// ---------------------------------------------
+
+function makePerson() {
+  let _bloodTypeList = ["A", "B", "O", "AB"];
+  let _bloodType = "A";
+
+  return {
+    set bloodTypeList(type) {
+      if (_bloodTypeList.includes(type)) {
+        _bloodType = type;
+      }
+    },
+
+    get bloodType() {
+      return _bloodType;
+    },
+  };
+}
+
+let person2 = makePerson();
+
+console.log(person2.bloodType);
+person2.bloodType = 'AB' // 의도된 변경
+console.log(person2.bloodType);
+person2.bloodType = 'C'  // 의도된 변경금지
+console.log(person2.bloodType);
+person2._bloodType = 'C' // 의도된 변경금지
+console.log(person2.bloodType);
