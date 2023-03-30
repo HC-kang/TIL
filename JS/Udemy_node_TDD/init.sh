b820c17a2f394e83fdf617ed504fb1345bbf5c9d
HC-kang <weston0713@gmail.com>
2023-03-30 21:43:58 +0900

    KATAs init

---
+++ JS/Udemy_node_TDD/init.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+npm init -y
+mkdir src
+mkdir test
+
+npm i typescript
+npm i -D jest
+npm i -D ts-jest
+npm i -D ts-node
+npm i -D @types/jest
+
+echo "{
+  \"compilerOptions\": {
+    \"module\": \"commonjs\",
+    \"noImplicitAny\": true,
+    \"removeComments\": true,
+    \"preserveConstEnums\": true,
+    \"sourceMap\": true,
+    \"target\": \"ES6\",
+    \"esModuleInterop\": true,
+    \"resolveJsonModule\": true,
+    \"outDir\": \"dist\"
+  },
+  \"include\": [\"src\",\"test\"],
+  \"exclude\": [\"node_modules\"]
+}
+" >> tsconfig.json
+
+echo "module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
+" >> jest.config.js
+
+echo "describe('abc', () => {
+  it('cdb', () => {
+    expect(1).toBe(1);
+  });
+});
+" >> test/example.test.ts
