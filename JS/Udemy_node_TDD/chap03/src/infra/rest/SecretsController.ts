c8ebf91c571ae5f0da69559cfbf4d9d8c1916182
HC-kang <weston0713@gmail.com>
2023-04-29 13:04:24 +0900

    Feat: write the first failing acceptance test

---
+++ JS/Udemy_node_TDD/chap03/src/infra/rest/SecretsController.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+
+export class SecretsController {
+  storeSecret = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    throw new Error('Method not implemented yet!');
+  };
+}
--- JS/Udemy_node_TDD/chap03/src/server.ts
+++ JS/Udemy_node_TDD/chap03/src/server.ts
@@ -1,20 +1,26 @@
-import { Secret } from "./domain/models/Secret";
-import { SecretRetriever } from "./services/SecretRetriever";
-import { UrlId } from "./domain/models/UrlId";
-import { Application } from "./infra/rest/Application";
-import { Route } from "./infra/rest/Route";
-import { SecretsByIdController } from "./infra/rest/SecretsByIdController";
-import { SecretsByIdRoute } from "./infra/rest/SecretsByIdRoute";
-import { OneTimeSecretRetriever } from "./services/OneTimeSecretRetriever";
-import { MongoSecretRepository } from "./infra/repositories/MongoSecretRepository";
+import { Secret } from './domain/models/Secret';
+import { SecretRetriever } from './services/SecretRetriever';
+import { UrlId } from './domain/models/UrlId';
+import { Application } from './infra/rest/Application';
+import { Route } from './infra/rest/Route';
+import { SecretsByIdController } from './infra/rest/SecretsByIdController';
+import { SecretsByIdRoute } from './infra/rest/SecretsByIdRoute';
+import { OneTimeSecretRetriever } from './services/OneTimeSecretRetriever';
+import { MongoSecretRepository } from './infra/repositories/MongoSecretRepository';
+import { SecretsController } from './infra/rest/SecretsController';
+import { SecretsRoute } from './infra/rest/SecretsRoute';
 
 const secretRepository = new MongoSecretRepository();
 const secretRetriever = new OneTimeSecretRetriever(secretRepository);
 const secretsByIdController = new SecretsByIdController(secretRetriever);
 const secretsByIdRoute = new SecretsByIdRoute(secretsByIdController);
 
+const secretsController = new SecretsController();
+const secretsRoute = new SecretsRoute(secretsController);
+
 const routeList: Route[] = [];
 routeList.push(secretsByIdRoute);
+routeList.push(secretsRoute);
 
 const application = new Application(routeList);
 
