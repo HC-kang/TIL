bd26fcc73c9eeab7829106e1d68bc4e42edb3b13
HC-kang <weston0713@gmail.com>
2023-05-01 14:24:06 +0900

    Feat: write end to end tests

--- JS/Udemy_node_TDD/chap03/src/domain/models/Secret.ts
+++ JS/Udemy_node_TDD/chap03/src/domain/models/Secret.ts
@@ -2,7 +2,7 @@ import { SecretValidationError } from "../errors/SecretValidationError";
 
 export class Secret {
   constructor(private secret: string) {
-    if (secret.length < 3)
+    if (secret.length <= 3)
       throw new SecretValidationError('Secret is too short');
   }
 
