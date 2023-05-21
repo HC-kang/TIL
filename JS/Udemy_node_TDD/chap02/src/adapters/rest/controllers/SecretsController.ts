eea6ff8e87a1ba96870167d71414d277be21bde7
HC-kang <weston0713@gmail.com>
2023-05-21 13:17:10 +0900

    Feat: REST controllers

---
+++ JS/Udemy_node_TDD/chap02/src/adapters/rest/controllers/SecretsController.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from 'express';
+import { SecretStorer } from '../../../domain/ports/in/SecretStorer';
+import { ValidationError } from './ValidationError';
+import { Secret } from '../../../domain/models/Secret';
+
+export class SecretsController {
+  constructor(private secretStorer: SecretStorer) {}
+
+  createSecret = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    try {
+      this.validateRequest(request);
+
+      const secret = new Secret(request.body.secret);
+      const urlId = await this.secretStorer.storeSecret(secret);
+      response.status(201).json(urlId);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  private validateRequest(request: Request) {
+    if (
+      !request.body ||
+      !request.body.secret ||
+      typeof request.body?.secret != 'string'
+    )
+      throw new ValidationError('Request body not valid');
+  }
+}
---
+++ JS/Udemy_node_TDD/chap02/test/adapters/rest/controllers/SecretsController.test.ts
@@ -0,0 +1,79 @@
+import { NextFunction, request, Request, response, Response } from 'express';
+import { ValidationError } from '../../../../src/adapters/rest/controllers/ValidationError';
+import { SecretStorer } from '../../../../src/domain/ports/in/SecretStorer';
+import { SecretsController } from '../../../../src/adapters/rest/controllers/SecretsController';
+import { UrlId } from '../../../../src/domain/models/UrlId';
+
+describe('Secrets Tests', () => {
+  it('should throw a validation error if the body of the request is not provided', async () => {
+    const req: Request = expect.any(request);
+    const res: Response = expect.any(response);
+    const next: NextFunction = jest.fn();
+
+    const secretStorer: SecretStorer = {
+      storeSecret: jest.fn(),
+    };
+
+    const secretsController = new SecretsController(secretStorer);
+    secretsController.createSecret(req, res, next);
+
+    expect(next).toBeCalledTimes(1);
+    expect(next).toBeCalledWith(new ValidationError('Request body not valid'));
+  });
+
+  it('should throw a validation error if the body does not have a secret', async () => {
+    const req: Request = expect.any(request);
+    req.body = { abc: 'abc' };
+    const res: Response = expect.any(response);
+    const next: NextFunction = jest.fn();
+
+    const secretStorer: SecretStorer = {
+      storeSecret: jest.fn(),
+    };
+
+    const secretsController = new SecretsController(secretStorer);
+    secretsController.createSecret(req, res, next);
+
+    expect(next).toBeCalledTimes(1);
+    expect(next).toBeCalledWith(new ValidationError('Request body not valid'));
+  });
+
+  it('should throw a validation error if the secret is not a string', async () => {
+    const req: Request = expect.any(request);
+    req.body = { secret: 123 };
+    const res: Response = expect.any(response);
+    const next: NextFunction = jest.fn();
+
+    const secretStorer: SecretStorer = {
+      storeSecret: jest.fn(),
+    };
+
+    const secretsController = new SecretsController(secretStorer);
+    secretsController.createSecret(req, res, next);
+
+    expect(next).toBeCalledTimes(1);
+    expect(next).toBeCalledWith(new ValidationError('Request body not valid'));
+  });
+
+  it('should create a valid secret', async () => {
+    const req: Request = expect.any(request);
+    req.body = { secret: '123qwe' };
+    const res: Response = expect.any(response);
+    res.status = jest.fn().mockReturnThis();
+    res.json = jest.fn();
+    const next: NextFunction = jest.fn();
+
+    const secretStorer: SecretStorer = {
+      storeSecret: jest.fn().mockResolvedValue(new UrlId('123456qwerty')),
+    };
+
+    const secretsController = new SecretsController(secretStorer);
+    await secretsController.createSecret(req, res, next);
+
+    expect(next).toBeCalledTimes(0);
+    expect(res.status).toBeCalledTimes(1);
+    expect(res.status).toBeCalledWith(201);
+    expect(res.json).toBeCalledTimes(1);
+    expect(res.json).toBeCalledWith({ urlId: '123456qwerty' });
+  });
+});
