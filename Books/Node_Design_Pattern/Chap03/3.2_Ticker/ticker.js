b258fadbb0bbf194a6d7195607c482be4c632eaa
HC-kang <weston0713@gmail.com>
2023-08-29 13:06:54 +0900

    📖 책너두 5기: Nodejs 디자인패턴 바이블 복습

--- Books/Node_Design_Pattern/Chap03/3.2_Ticker/ticker.js
+++ Books/Node_Design_Pattern/Chap03/3.2_Ticker/ticker.js
@@ -1,21 +1,39 @@
 import { EventEmitter } from 'events';
 
 function ticker(number, cb) {
-  const eventEmitter = new EventEmitter();
+  const emitter = new EventEmitter();
+
   const step = 50;
-  let cnt = 0;
+  let count = 0;
+
+  // process.nextTick(() => emitter.emit('tick', 'start tick!!!')); // ----> 3.3
+  process.nextTick(() => {
+    checkError(); // ----> 3.4
+    emitter.emit('tick', 'start tick!!!')
+  });
+
+  const checkError = () => {  // ----> 3.4
+    const timestamp = Date.now();
+    if (timestamp % 5 === 0) {
+      const error = new Error(`Error at ${timestamp}`);
+      emitter.emit('error', error);
+    }
+  }
+
   const repeat = (n) => {
-    if (n < step) {
-      return cb(`Done. Ticked: ${cnt}`);
+    if (n <= step) {
+      return cb(`Done!! Ticked: ${count}`);
     }
     setTimeout(() => {
-      eventEmitter.emit('tick', `Ticked: ${cnt++}`);
+      checkError(); // ----> 3.4
+      emitter.emit('tick', `Ticked: ${count++}`);
       repeat(n - step);
     }, step);
-  };
+  }
   repeat(number);
-  return eventEmitter;
+  return emitter;
 }
-const cb = (count) => console.log(`Tik-Tok: ${count}`);
 
-ticker(500, cb).on('tick', console.log);
+const cb = (count) => console.log(count);
+
+ticker(500, cb).on('tick', cb);
