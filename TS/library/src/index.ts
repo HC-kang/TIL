15aaf6a5e03ee816dbb4932731ed8085c0be0bd9
HC-kang <weston0713@gmail.com>
2023-09-04 09:04:45 +0900

    Feat: add main func

--- TS/library/src/index.ts
+++ TS/library/src/index.ts
@@ -38,62 +38,94 @@ interface RentManager {
   getBooks(): Book[];
   addBook(user: User, book: Book): void;
   removeBook(user: User, book: Book): void;
-  rendBook(user: User, book: Book): void;
+  rentBook(user: User, book: Book): void;
   returnBook(user: User, book: Book): void;
 }
 
 class Library implements RentManager {
   private books: Book[] = [];
+  // rentedBooks는 유저의 대여 이력을 관리해요!
   private rentedBooks: Map<string, Book> = new Map<string, Book>();
 
   getBooks(): Book[] {
+    // 깊은 복사를 하여 외부에서 books를 수정하는 것을 방지합니다.
     return JSON.parse(JSON.stringify(this.books));
   }
 
   addBook(user: User, book: Book): void {
     if (user.getRole() !== Role.LIBRARIAN) {
-      throw new Error("Only librarian can add book");
+      console.log("사서만 도서를 추가할 수 있습니다.");
+      return;
     }
-    console.log(`${user.name} add ${book.title}`);
+
     this.books.push(book);
   }
 
   removeBook(user: User, book: Book): void {
     if (user.getRole() !== Role.LIBRARIAN) {
-      throw new Error("Only librarian can remove book");
+      console.log("사서만 도서를 삭제할 수 있습니다.");
+      return;
     }
-    if (!this.books.find((b) => b.title === book.title)) {
-      throw new Error("Book not found");
+
+    const index = this.books.indexOf(book);
+    if (index !== -1) {
+      this.books.splice(index, 1);
     }
-    console.log(`${user.name} remove ${book.title}`);
-    this.books = this.books.filter((b) => b.title !== book.title);
   }
 
-  rendBook(user: User, book: Book): void {
+  rentBook(user: User, book: Book): void {
     if (user.getRole() !== Role.MEMBER) {
-      throw new Error("Only member can rend book");
+      console.log("유저만 도서를 대여할 수 있습니다.");
+      return;
     }
-    if (!this.books.find((b) => b.title === book.title)) {
-      throw new Error("Book not found");
-    }
+
     if (this.rentedBooks.has(user.name)) {
-      throw new Error("User already rent book");
+      console.log(
+        `${user.name}님은 이미 다른 책을 대여중이라 빌릴 수 없습니다.`
+      );
+    } else {
+      this.rentedBooks.set(user.name, book);
+      console.log(`${user.name}님이 [${book.title}] 책을 빌렸습니다.`);
     }
-    this.rentedBooks.set(user.name, book);
-    console.log(`${user.name} rend ${book.title}`);
   }
 
   returnBook(user: User, book: Book): void {
     if (user.getRole() !== Role.MEMBER) {
-      throw new Error("Only member can return book");
+      console.log("유저만 도서를 반납할 수 있습니다.");
+      return;
     }
-    if (!this.rentedBooks.has(user.name)) {
-      throw new Error("User not rent book");
+
+    if (this.rentedBooks.get(user.name) === book) {
+      this.rentedBooks.delete(user.name);
+      console.log(`${user.name}님이 [${book.title}] 책을 반납했어요!`);
+    } else {
+      console.log(`${user.name}님은 [${book.title}] 책을 빌린적이 없어요!`);
     }
-    if (this.rentedBooks.get(user.name)?.title !== book.title) {
-      throw new Error("User not rent this book");
-    }
-    this.rentedBooks.delete(user.name);
-    console.log(`${user.name} return ${book.title}`);
   }
 }
+
+function main() {
+  const myLibrary = new Library();
+  const librarian = new Librarian("김사서", 30);
+  const member1 = new Member("김회원", 20);
+  const member2 = new Member("박회원", 25);
+
+  const book = new Book("타입스크립트 완전 정복", "박사서", new Date());
+  const book2 = new Book("타입스크립트 디자인 패턴", "권사서", new Date());
+  const book3 = new Book("타입스크립트 실전 프로그래밍", "강사서", new Date());
+
+  myLibrary.addBook(librarian, book);
+  myLibrary.addBook(librarian, book2);
+  myLibrary.addBook(librarian, book3);
+
+  const books = myLibrary.getBooks();
+  console.log('도서 목록', books);
+
+  myLibrary.rentBook(member1, book);
+  myLibrary.rentBook(member2, book);
+
+  myLibrary.returnBook(member1, book);
+  myLibrary.returnBook(member2, book);
+}
+
+main();
