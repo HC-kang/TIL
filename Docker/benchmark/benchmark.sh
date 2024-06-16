41d1ee4908892301084c67ace47b9e9c2177cc65
HC-kang <weston0713@gmail.com>
2024-06-16 22:19:53 +0900

    Docker Benchmarking

--- Docker/benchmark/benchmark.sh
+++ Docker/benchmark/benchmark.sh
@@ -5,23 +5,28 @@ run_benchmark() {
     local output_prefix=$2
 
     DURATION="1m"
-    THREADS_NETWORK_DISK=32
-    CONNECTIONS_NETWORK_DISK=400
-    THREADS_CPU=2
-    CONNECTIONS_CPU=4
 
-    OUTPUT_DIR="./${output_prefix}"
+    OUTPUT_DIR="./Part2/${output_prefix}"
     mkdir -p $OUTPUT_DIR
 
     echo "Benchmarking ${output_prefix} environment..."
 
-    wrk -t$THREADS_NETWORK_DISK -c$CONNECTIONS_NETWORK_DISK -d$DURATION ${base_url}/network > $OUTPUT_DIR/${output_prefix}_network.txt
+    THREADS=32
+    CONNECTIONS=400
+
+    wrk -t$THREADS -c$CONNECTIONS -d$DURATION ${base_url}/network > $OUTPUT_DIR/${output_prefix}_network.txt
     echo "Network benchmark completed."
 
-    wrk -t$THREADS_NETWORK_DISK -c$CONNECTIONS_NETWORK_DISK -d$DURATION ${base_url}/disk-io > $OUTPUT_DIR/${output_prefix}_disk_io.txt
+    THREADS=3
+    CONNECTIONS=4
+
+    wrk -t$THREADS -c$CONNECTIONS -d$DURATION ${base_url}/disk-io > $OUTPUT_DIR/${output_prefix}_disk_io.txt
     echo "Disk I/O benchmark completed."
 
-    wrk -t$THREADS_CPU -c$CONNECTIONS_CPU -d$DURATION ${base_url}/cpu > $OUTPUT_DIR/${output_prefix}_cpu.txt
+    THREADS=5
+    CONNECTIONS=5
+
+    wrk -t$THREADS -c$CONNECTIONS -d$DURATION ${base_url}/cpu > $OUTPUT_DIR/${output_prefix}_cpu.txt
     echo "CPU benchmark completed."
 
     echo "Benchmark completed. Results saved to ${output_prefix}_network.txt, ${output_prefix}_disk_io.txt, ${output_prefix}_cpu.txt."
@@ -35,15 +40,15 @@ read -p "Enter choice [1-3]: " choice
 
 case $choice in
     1)
-        BASE_URL="http://127.0.0.1:3000"
+        BASE_URL="http://3.36.78.116:3000"
         run_benchmark $BASE_URL "native"
         ;;
     2)
-        BASE_URL="http://host.docker.internal:3000"
+        BASE_URL="http://3.36.78.116:3000"
         run_benchmark $BASE_URL "host"
         ;;
     3)
-        BASE_URL="http://bridge.docker.internal:3000"
+        BASE_URL="http://3.36.78.116:3000"
         run_benchmark $BASE_URL "bridge"
         ;;
     *)
