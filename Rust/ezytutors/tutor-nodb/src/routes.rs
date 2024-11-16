83fc433f1db1f10d9ab3412bd3016a90fea2f415
HC-kang <weston0713@gmail.com>
2024-11-16 23:13:23 +0900

    Rust actix state

---
+++ Rust/ezytutors/tutor-nodb/src/routes.rs
@@ -0,0 +1,6 @@
+use super::handlers::*;
+use actix_web::web;
+
+pub fn general_routes(cfg: &mut web::ServiceConfig) {
+    cfg.route("/health", web::get().to(health_check_handler));
+}
---
+++ Rust/ezytutors/tutor-nodb/src/state.rs
@@ -0,0 +1,6 @@
+use std::sync::Mutex;
+
+pub struct AppState {
+    pub health_check_response: String,
+    pub visit_count: Mutex<u32>,
+}
