9859beed91efb426aecec7e7e3fa4452168c30a8
HC-kang <weston0713@gmail.com>
2024-11-24 15:46:34 +0900

    Feat: add data model

--- Rust/ezytutors/tutor-nodb/src/bin/tutor-service.rs
+++ Rust/ezytutors/tutor-nodb/src/bin/tutor-service.rs
@@ -5,6 +5,9 @@ use std::sync::Mutex;
 #[path = "../handlers.rs"]
 mod handlers;
 
+#[path = "../models.rs"]
+mod models;
+
 #[path = "../routes.rs"]
 mod routes;
 
@@ -19,6 +22,7 @@ async fn main() -> io::Result<()> {
     let shared_data = web::Data::new(AppState {
         health_check_response: "I'm good. You'v already asked me".to_string(),
         visit_count: Mutex::new(0),
+        courses: Mutex::new(vec![]),
     });
     let app = move || {
         App::new()
