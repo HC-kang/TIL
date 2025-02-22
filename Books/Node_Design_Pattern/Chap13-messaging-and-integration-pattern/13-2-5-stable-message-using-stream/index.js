773d048ce6484f82c29477b56d89a6909abfd8dd
HC-kang <weston0713@gmail.com>
2025-02-22 22:06:24 +0900

    feat: 13-2-5 stable message using stream

---
+++ Books/Node_Design_Pattern/Chap13-messaging-and-integration-pattern/13-2-5-stable-message-using-stream/index.js
@@ -0,0 +1,53 @@
+import { createServer } from 'http';
+import staticHandler from 'serve-handler';
+import { WebSocketServer, WebSocket } from 'ws';
+import Redis from 'ioredis';
+
+const redisClient = new Redis();
+const redisClientXRead = new Redis();
+
+const server = createServer((req, res) => {
+  return staticHandler(req, res, { public: './www' });
+});
+
+const wss = new WebSocketServer({ server });
+wss.on('connection', async client => {
+  console.log('Client connected');
+
+  client.on('message', msg => {
+    console.log(`Message: ${msg}`);
+    redisClient.xadd('chat_stream', '*', 'message', msg);
+  });
+
+  const logs = await redisClient.xrange('chat_stream', '-', '+')
+  for (const [, [, message]] of logs) {
+    client.send(message);
+  }
+})
+
+function broadcast(msg) {
+  for (const client of wss.clients) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(msg);
+    }
+  }
+}
+
+let lastRecordId = '$';
+
+async function processStreamMessages() {
+  while (true) {
+    const [[, records]] = await redisClientXRead.xread(
+      'BLOCK', '0', 'STREAMS', 'chat_stream', lastRecordId
+    );
+    for (const [recordId, [, message]] of records) {
+      console.log(`Message from stream: ${message}`);
+      broadcast(message);
+      lastRecordId = recordId;
+    }
+  }
+}
+
+processStreamMessages().catch(err => console.error(err));
+
+server.listen(process.argv[2] || 8080);
